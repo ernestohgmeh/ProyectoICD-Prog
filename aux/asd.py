94d99758108c1a2502d2cd484455b33e3fd5e0cd
Ernesto Herrera <ernestohg2002@gmail.com>
2024-12-12 03:23:50 -0500

    Update primeros graficos

--- aux/asd.py
+++ aux/asd.py
@@ -10,8 +10,8 @@ def Load_Files()->list:
     for files in os.listdir(path_files):
         the_file = os.path.join(path_files,files)
         with open(the_file, 'r', encoding='utf-8') as opened:
-            if opened["minicipality"] == "String/ Nombre del municipio": continue
             texto = json.load(opened)
+            if texto["municipality"] == "String/ Nombre del municipio": continue
             output.append(texto)
     return output
 
